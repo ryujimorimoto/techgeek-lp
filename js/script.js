8ce6d2371b15a9a902b65fa0d1b29932a5b3da77
honda yurika <rain.is.capriccio@gmail.com>
2021-11-01 14:11:30 +0900

    非表示コンテンツなど修正

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
-  /*--------------------------
-  ナビゲーションメニュー開閉
-  ----------------------------*/
+/*--------------------------
+ナビゲーションメニュー開閉
+----------------------------*/
 
-  function navToggle() {
-    document.querySelector('html').classList.toggle('open');
-    document.getElementById('modal-gray').classList.toggle('modal-open');
-  }
+function navToggle() {
+  document.querySelector('html').classList.toggle('open');
+  document.getElementById('modal-gray').classList.toggle('modal-open');
+}
 
-  function closeNav() {
-    document.querySelector('html').classList.remove('open');
-    document.getElementById('modal-gray').classList.toggle('modal-open');
-  }
+function closeNav() {
+  document.querySelector('html').classList.remove('open');
+  document.getElementById('modal-gray').classList.toggle('modal-open');
+}
 
-  /*---------------------------------
-  アコーディオンメニュー
-  ----------------------------------*/
-  window.addEventListener("load", loaded => {
-  [].slice.call(document.getElementsByClassName("js-accordion-trigger"))
-    .map( elm => elm.addEventListener("click", () => {
-        elm.classList.toggle("is-active");
-        elm.querySelector(".accordion__content").classList.toggle("is-open");
-      }
-      )
-    );
+/*---------------------------------
+（header）ページ内スムーススクロール
+----------------------------------*/
+
+
+/*----------------------------------------------
+(なぜ、shopify?)(よくあるご質問)アコーディオンメニュー
+-----------------------------------------------*/
+window.addEventListener("load", loaded => {
+[].slice.call(document.getElementsByClassName("js-accordion-trigger"))
+  .map( elm => elm.addEventListener("click", () => {
+      elm.classList.toggle("is-active");
+      elm.querySelector(".accordion__content").classList.toggle("is-open");
+    }
+    )
+  );
 });
 
 /*---------------------------------
-  コースタブ切り替え
-  ----------------------------------*/
+(料金)コースタブ切り替え
+----------------------------------*/
 function tabSwitch(selectTab) {
   document.getElementsByClassName('course-active')[0].classList.remove('course-active');
   document.getElementsByClassName('course-show')[0].classList.remove('course-show');
